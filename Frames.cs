a9f879ef73b34d26a71607aa5a10317a945f494f
Matthias van Hogerhuis <matthiasvanhogerhuis017@gmail.com>
2026-02-24 11:28:20 +0100

    initial commit

---
+++ Frames.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadApple
+{
+    internal class Frames
+    {
+        public static void Start()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 40; j++)
+                {
+                    Console.Write("+");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
